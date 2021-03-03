7ff1df25620f9a89c746ca9579693afcd308301d
Sam D <samidiaz3@gmail.com>
2021-03-03 02:41:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    #@@all << self
+    save
+  end
+
+  def self.all  # returns Dog's array @@all
+    @@all
+  end
+
+  def self.print_all #prints all Dog's name
+    @@all.collect do |dog|
+      puts dog.name
+    end
+  end
+
+  def save #instance method, not class (self.) method. This way it runs per instance, called upon from initialize method
+    @@all << self
+  end
+
+  def self.clear_all #clearing @@all=[array]
+    @@all.clear
+  end
+
+
+end
